5be757b4a6fd69d1dcd371c358ab79837bba32e4
Suradianzah08 <suradianzah_nugroho_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:02:26 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,36 @@
+public class Hello{
+    public static void  main(String[] args) {
+        
+        String nama="Suradianzah";
+        String asal_Kota="malang";
+        int usia=15;
+        double beratBadan=50.00;
+        float tinggiBadan=172.0f;
+        char jenisKelamin= 'L';
+        boolean status = true;
+        
+        
+
+    
+
+        System.out.println("nama: "+nama);
+        System.out.println("usia: "+usia);
+        System.out.println("berat badan: "+beratBadan);
+        System.out.println("tinggi badan: "+tinggiBadan);
+        System.out.println("jenis kelamin: "+jenisKelamin);
+        System.out.println("Status: "+status);
+        
+
+        
+        
+        
+        
+        
+        
+       
+    }
+    }
+    
+    
+ 
+
---
+++ Tambah.java
@@ -0,0 +1,10 @@
+public class Tambah {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 90;
+    
+        int hasil = a+b;
+        System.out.println("Hasil dari " + a + " ditambah a " + b + " adalah " + hasil);
+    }
+}
+   
